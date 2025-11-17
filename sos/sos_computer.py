7b9b734ad5884ac5e68f4f1afef6383460f80bdd
KZhangCodes <kzkm8@umsystem.edu>
2025-11-17 02:56:26 -0600

    computer opponent now checks for scoring moves, improved

--- sos/sos_computer.py
+++ sos/sos_computer.py
@@ -17,7 +17,36 @@ class EasyComputerOpponent(ComputerOpponent):
         board = game.board
         size = board.board_size
 
-        empty_cells = [
+        scoring_moves: list[Tuple[int, int, str]] = [] #store scoring moves
+        empty_cells: list[Tuple[int, int]] = []
+        original_player = game.current_player
+        #loop through cells
+        for row in range(size):
+            for col in range(size):
+                if board.is_empty(row, col):
+                    empty_cells.append((row, col)) #track empty cells
+                    game.current_player = self.side
+                    #line segment completion check
+                    s_line = len(game.new_lines_from_move(row, col, "S"))
+                    if s_line > 0:
+                        scoring_moves.append((row, col, "S"))
+                    o_line = len(game.new_lines_from_move(row, col, "O"))
+                    if o_line > 0:
+                        scoring_moves.append((row, col, "O"))
+
+        game.current_player = original_player
+        #choose random scoring move
+        if scoring_moves:
+            return random.choice(scoring_moves)
+        if not empty_cells:
+            raise RuntimeError("Not empty")
+        #choose random cell and letter
+        row, col = random.choice(empty_cells)
+        letter = random.choice(["S", "O"])
+        return row, col, letter
+
+
+        '''empty_cells = [
             (row, col)
             for row in range(size)
             for col in range(size)
@@ -30,4 +59,4 @@ class EasyComputerOpponent(ComputerOpponent):
         row, col = random.choice(empty_cells)
         letter = random.choice(["S", "O"])
 
-        return row, col, letter
+        return row, col, letter'''
